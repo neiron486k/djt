755277f6570fa51646ec73bfe2bd58bc4d69be10
Alexey Astafev <efsneiron@gmail.com>
2020-02-06 16:14:00 +0300

    added model to admin

--- pages/models.py
+++ pages/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=50, null=True)
-    description = models.CharField(max_length=255, null=True)
-    keywords = models.CharField(max_length=255, null=True)
-    content = models.TextField()
     slug = models.CharField(max_length=50, db_index=True, unique=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=Truea)
+
+    def __str__(self):
+        return self.slug
 
     class Meta:
         db_table = 'pages'
